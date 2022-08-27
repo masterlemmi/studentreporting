911bda50f2f5d262a76c25ea5fd98caa50c0ff1b
lemuel.taeza <lemuel.taeza@oracle.com>
2022-08-27 17:45:42 +0800

    fix edit subj

--- admin/modules/subject/edit.php
+++ admin/modules/subject/edit.php
@@ -83,15 +83,10 @@ $object = $singlesubject->single_subject($subjid);
           "ay">Academic Year</label>
 
           <div class="col-md-8">
-            <select class="form-control input-sm" name="ay" id="ay">
-				<option value="2013-2014">2013-2014</option>
-				<option value="2014-2015">2014-2015</option>
-				<option value="2015-2016">2015-2016</option>
-				<option value="2016-2017">2016-2017</option>
-				<option value="2017-2018">2017-2018</option>
-				<option value="2018-2019">2018-2019</option>
-				<option value="2019-2020">2019-2020</option>	
-			</select>	
+
+
+              <input class="form-control input-sm" id="ay" name="ay" placeholder=
+                  "Academic Year"  value="<?php echo $object->AY;?>">        
           </div>
         </div>
       </div>
